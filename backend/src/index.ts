78b8e39bd5bd034436a28cf70bf9fa5141e367a1
ayush gajera <ayushgajera50@gmail.com>
2025-07-28 22:39:56 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,9 +18,9 @@ app.use(express.json());
 const _dirname1 = path.resolve();
 app.use(express.static(path.join(_dirname1, "../pdf-ai-frontend/dist"))); // or build
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(_dirname1, "../pdf-ai-frontend/dist/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(_dirname1, "../pdf-ai-frontend/dist/index.html"));
+// });
 
 
 const upload = multer({ dest: "uploads/" });
